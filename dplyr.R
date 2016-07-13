d0089f58b468cf9bd39f7f998119eaa79136049f
agisga <alexej.go@googlemail.com>
2016-07-13 10:04:10 -0700

    join functionality in dplyr

--- dplyr.R
+++ dplyr.R
@@ -76,3 +76,23 @@ str(qwer)
 attributes(qwer)
 # what is the average life expectancy by continent?
 gtbl %>% group_by(continent) %>% summarize(aveLife = mean(lifeExp))
+
+#--- join
+superheroes <- c("name, alignment, gender, publisher",
+                 "Magneto, bad, male, Marvel",
+                 "Storm, good, female, Marvel",
+                 "Mystique, bad, female, Marvel",
+                 "Batman, good, male, DC",
+                 "Joker, bad, male, DC",
+                 "Catwoman, bad, female, DC",
+                 "Hellboy, good, male, Dark Horse Comics")
+superheroes <- read.csv(text = superheroes, strip.white = TRUE, as.is=TRUE)
+
+publishers <- c("publisher, yr_founded",
+                "       DC, 1934",
+                "   Marvel, 1939",
+                "    Image, 1992")
+publishers <- read.csv(text = publishers, strip.white = TRUE, as.is=TRUE)
+inner_join(superheroes, publishers)
+left_join(superheroes, publishers)
+full_join(superheroes, publishers)
